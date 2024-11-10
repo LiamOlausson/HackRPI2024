60b35dc0f3a0cc31ed685d6ede38a9420ebe2098
Liam Olausson <liam.olausson@acurix.com>
2024-11-09 20:15:30 -0500

    Ending development on the localserver
    Accessibility options added:
    - Reading the entire board
    - Reading specific squares
    - Hidden play
    - Reading the options
    - Early cancellation of the reading

---
+++ chessPlugin.js
@@ -0,0 +1,16 @@
+// Find the dimenstion of the chessboard
+// Use a cursor click to click the pieces to other squares
+
+
+
+//Tag for the board pixels
+
+//#board-single > svg.arrows
+//svg 0 0 100 100
+
+// enter keypresses as a ajacent window overlayed on the website which then will use cursor logic to move the pieces to the desired locations
+
+//Problems
+// Window size
+// Multiple displays
+// 
